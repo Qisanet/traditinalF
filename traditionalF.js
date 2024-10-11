1c6b5cb3afb39e2df186ebedb7fba445efcac210
Qisanet <ksanetang@gmail.com>
2024-10-11 12:55:04 +0300

    Add files via upload

---
+++ traditionalF.js
@@ -0,0 +1,18 @@
+// function add(a,b){
+//     return a+b;
+// }
+const add = (a, b) => a + b;
+console.log(add(3, 4));
+
+
+// function multiply(a,b){
+//     return a+b;
+// 
+const  multiply =(a, b) => a + b;
+console.log(add(3, 4));
+
+// function greet(name){
+//     return 'Hello,'+name;
+// }
+const greet = (name) => 'Hello, ' + name;
+console.log(greet('Alice')); 
